bb50950f6707e7013e0f58803a8f30f3125d1ea9
Tamzeed Quazi <tamzeed.q@gmail.com>
2024-12-27 10:32:21 -0800

    reset

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,66 +1,43 @@
+import { Tabs } from 'expo-router';
 import React from 'react';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Tabs } from 'expo-router';
-import { Pressable, StyleSheet } from 'react-native';
+import { Platform } from 'react-native';
 
-import Colors, { BLACK, GREY, RED, WHITE } from '@/constants/Colors';
-import { useColorScheme } from '@/components/useColorScheme';
-import { useClientOnlyValue } from '@/components/useClientOnlyValue';
-
-// You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
-function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
-  color: string;
-}) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
-}
+import { HapticTab } from '@/components/HapticTab';
+import { IconSymbol } from '@/components/ui/IconSymbol';
+import TabBarBackground from '@/components/ui/TabBarBackground';
+import { Colors } from '@/constants/Colors';
+import { useColorScheme } from '@/hooks/useColorScheme';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
-  
+
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: RED,
-        tabBarInactiveTintColor: WHITE,
-        tabBarStyle: {
-          backgroundColor: GREY,
-        },
-        // tabBarLabelStyle: {
-        //   fontSize: 12,
-        //   fontWeight: '600',
-        // },
-        // tabBarIconStyle: {
-        //   marginBottom: -5,
-        // },
-        headerShown: useClientOnlyValue(false, true),
+        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        headerShown: false,
+        tabBarButton: HapticTab,
+        tabBarBackground: TabBarBackground,
+        tabBarStyle: Platform.select({
+          ios: {
+            // Use a transparent background on iOS to show the blur effect
+            position: 'absolute',
+          },
+          default: {},
+        }),
       }}>
       <Tabs.Screen
-        name="closet"
+        name="index"
         options={{
-          title: 'Closet',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="plus"
-                    size={25}
-                    color={Colors[colorScheme ?? 'light'].text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
-          ),
+          title: 'Home',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
         }}
       />
       <Tabs.Screen
-        name="activity"
+        name="explore"
         options={{
-          title: 'Activity',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          title: 'Explore',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
     </Tabs>
--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,25 +1,26 @@
-const tintColorLight = '#2f95dc';
+/**
+ * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
+ * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
+ */
+
+const tintColorLight = '#0a7ea4';
 const tintColorDark = '#fff';
 
-export const BACKGROUND = "#ccc5b9";
-export const WHITE = "#fffcf2";
-export const GREY = "#403d39";
-export const BLACK = "#252422";
-export const RED = "#c1292e";
-
-export default {
+export const Colors = {
   light: {
-    text: '#000',
+    text: '#11181C',
     background: '#fff',
     tint: tintColorLight,
-    tabIconDefault: '#ccc',
+    icon: '#687076',
+    tabIconDefault: '#687076',
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#fff',
-    background: '#000',
+    text: '#ECEDEE',
+    background: '#151718',
     tint: tintColorDark,
-    tabIconDefault: '#ccc',
+    icon: '#9BA1A6',
+    tabIconDefault: '#9BA1A6',
     tabIconSelected: tintColorDark,
   },
 };
